30b244ace0009d1b67793fd504fb3908eac8ef5e
shuajay <joshuajayphiri5@gmail.com>
2022-07-14 01:09:28 +0200

    added the application factory that allows creation of many instances of the application

--- lms/__init__.py
+++ lms/__init__.py
@@ -1,1 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+#init the database
+db = SQLAlchemy()
+
+#application factory
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'bud1r1r0l1brary'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///librarymanagementsystem.sqlite'
+
+    db.init_app(app)
