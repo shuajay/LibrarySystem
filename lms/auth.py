d7aadad1668cf22fe7378dee13e6d517cc99cf5a
shuajay <joshuajayphiri5@gmail.com>
2022-07-14 01:37:07 +0200

    added the auth routes

---
+++ lms/auth.py
@@ -0,0 +1,18 @@
+#authentication routes for the applucation
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+
+#initialize the blueprint
+auth = Blueprint('auth', __name__)
+
+#route for login
+@auth.route('/login')
+def login():
+    return 'Login'
+
+@auth.route('/addUser')
+def addUser():
+    return 'add user'
+
+@auth.route('/logout')
+def logout():
+    return 'log out'
